ed806a921533f204cd03bc22131a040f21d3d72d
Taylor Zanke <taylor.zanke@gmail.com>
2021-06-11 17:11:32 -0700

    add initial animation:

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle, theme } from '../styles/theme'
 import { useRouter } from 'next/router'
+import { AnimatePresence, motion } from "framer-motion"
 import Head from 'next/head'
 
 import { Layout } from '../components/ui'
@@ -18,10 +19,23 @@ function App({
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
       </Head>
       <GlobalStyle />
-      <Header />
-      <Layout>
-        <Component key={router.pathname} {...pageProps} />
-      </Layout>
+
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          transition={{duration: 1}}
+        >
+
+          <Header />
+          <Layout>
+            <Component key={router.pathname} {...pageProps} />
+          </Layout>
+
+        </motion.div>
+      </AnimatePresence>
+
     </ThemeProvider>
   )
 }
